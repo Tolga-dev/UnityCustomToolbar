a5a00696e47c9ea6482105ca6d87ea5545d65307
Tolga-dev <orhan.konat.12@gmail.com>
2024-05-19 18:27:23 +0300

    Init

---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+        
+    }
+
+    void Update()
+    {
+        
+        
+    }
+}
