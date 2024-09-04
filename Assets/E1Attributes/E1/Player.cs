1255df02c5cd23dd037b8fb98b995ca7f965e6d5
Tolga-dev <orhan.konat.12@gmail.com>
2024-09-05 01:24:38 +0300

    Object attributes

--- Assets/E1Attributes/E1/Player.cs
+++ Assets/E1Attributes/E1/Player.cs
@@ -1,18 +1,16 @@
 using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-namespace Attributes.E1
+namespace E1Attributes.E1
 {
     public class Player : MonoBehaviour
     {
-        [TextArea(10, 20)] 
-        public string description;
+        [TextArea(10, 20)] public string description;
 
         [SerializeField] private int iamAPrivate = 31;
 
         [HideInInspector] public int iamAPublic;
-        
+
         [field: SerializeField] public int MyProperty { get; set; }
 
         [Range(0, 360)] public int degree;
