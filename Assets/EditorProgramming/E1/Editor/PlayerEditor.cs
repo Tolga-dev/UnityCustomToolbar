efadd6b5251a51521b19272effc87ab516fb0f79
Tolga-dev <orhan.konat.12@gmail.com>
2024-09-28 18:03:40 +0300

    Adding extra functions

--- Assets/EditorProgramming/E1/Editor/PlayerEditor.cs
+++ Assets/EditorProgramming/E1/Editor/PlayerEditor.cs
@@ -1,10 +1,17 @@
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace EditorProgramming.E1.Editor
 {
+    public enum MyEnum
+    {
+        None = 0, // Custom name for "Nothing" option
+        A = 1,
+        B = 2,
+        All = ~0, // Custom name for "Everything" option
+    }
+    
     [CustomEditor(typeof(Player))]
     public class PlayerEditor : UnityEditor.Editor
     {
@@ -22,6 +29,15 @@ namespace EditorProgramming.E1.Editor
         
         AnimationCurve curveX = AnimationCurve.Linear(0, 0, 10, 10);
 
+        public double dValue = 0.0f;
+        public float fValue = 0.0f;
+        public int iValue = 0;
+        public string tValue = "Hello";
+        
+        public int toolbarInt = 0;
+        public string[] toolbarStrings = {"Toolbar1", "Toolbar2", "Toolbar3"};
+
+        public MyEnum myEnum = MyEnum.A;
         public void OnEnable()
         {
             Debug.Log("On Enable is called");
@@ -58,15 +74,69 @@ namespace EditorProgramming.E1.Editor
             EditorGUILayout.Separator();
             VerticalMaker();
             EditorGUILayout.Separator();
+            
             EditorGUILayout.ColorField(Color.black);
             EditorGUILayout.Separator();
+            
             EditorGUILayout.CurveField("Animation on X", curveX);
             EditorGUILayout.Separator();
+
+            EditorGUILayout.DelayedDoubleField("A value",dValue);
+            EditorGUILayout.DoubleField("A value",dValue);
+            EditorGUILayout.DelayedFloatField("A value",fValue);
+            EditorGUILayout.DelayedIntField("A value",iValue);
+            EditorGUILayout.DelayedTextField("A value",tValue);
+            
+            EditorGUILayout.Separator();
+            DropDownMaker();
+            
+            EditorGUILayout.Separator();
+            toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings);
+            
+            EditorGUILayout.Separator();
+            EndEditorMaker();
+            
+            EditorGUILayout.Separator();
+            myEnum = (MyEnum)EditorGUILayout.EnumFlagsField(myEnum);
             
             DrawDefaultInspector();
         }
+        
+        private void EndEditorMaker()
+        {
+            BuildTargetGroup selectedBuildTargetGroup = EditorGUILayout.BeginBuildTargetSelectionGrouping();
+            if (selectedBuildTargetGroup == BuildTargetGroup.Android)
+            {
+                EditorGUILayout.LabelField("Android specific things");
+            }
 
+            if (selectedBuildTargetGroup == BuildTargetGroup.Standalone)
+            {
+                EditorGUILayout.LabelField("Standalone specific things");
+            }
 
+            EditorGUILayout.EndBuildTargetSelectionGrouping();
+        }
+
+        private void DropDownMaker()
+        {
+            if (EditorGUILayout.DropdownButton(new GUIContent("Options"), FocusType.Keyboard))
+            {
+                GenericMenu menu = new GenericMenu();
+
+                menu.AddItem(new GUIContent("Option 1"), false, OnOptionSelected, "Option 1");
+                menu.AddItem(new GUIContent("Option 2"), false, OnOptionSelected, "Option 2");
+                menu.AddItem(new GUIContent("Option 3"), false, OnOptionSelected, "Option 3");
+
+                menu.ShowAsContext();
+            }
+        }
+
+        private void OnOptionSelected(object option)
+        {
+            Debug.Log("Selected: " + option);
+        }
+        
         private void VerticalMaker()
         {
             Rect r = EditorGUILayout.BeginVertical("Button");
